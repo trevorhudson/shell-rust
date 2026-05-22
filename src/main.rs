09e3ed428809e2b82142b6edb8f452de79115b10
Trevor Hudson <trevor.c.hudson@gmail.com>
2026-05-21 20:20:10 -0700

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,40 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{env, path::PathBuf};
+use std::{env, os::unix::process::CommandExt, path::PathBuf, process::Command};
 
 use is_executable::IsExecutable;
 
 // Now let's refactor to make this actually functional!
-
-fn main() {
+// implement better control flow for commands
+//
+fn main() -> Result<(), anyhow::Error> {
     loop {
         let mut command = String::new();
+
         print!("$ ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut command).unwrap();
-        command = command.trim().to_string();
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut command)?;
 
-        if command == "exit" {
+        let mut iter = command.trim().split_ascii_whitespace();
+        let Some(program) = iter.next() else {
+            continue;
+        };
+
+        if program == "exit" {
             break;
-        } else if let Some(argument) = command.strip_prefix("type ") {
-            if argument == "echo" || argument == "exit" || argument == "type" {
-                println!("{} is a shell builtin", argument);
-                continue;
-            }
+        }
 
-            match locate_executable(argument) {
-                None => println!("{}: not found", argument),
-                Some(val) => {
-                    println!("{}: found", val.display());
-                    if let Some(arguments) = command.strip_suffix(argument) {
-                        println!("{}: found", arguments)
-                    }
-                    // let new_command = Command::new(val).args(args)
-                }
-            };
-        } else if let Some(arguments) = command.strip_prefix("echo ") {
-            println!("{}", arguments);
-        } else {
-            println!("{}: command not found", command);
+        match locate_executable(program) {
+            Some(path) => {
+                let mut c = Command::new(path);
+                println!("{:?}", c.get_program());
+                c.args(iter);
+                c.status()?;
+            }
+            None => println!("{}: command not found", command),
         }
     }
+    Ok(())
 }
 
 fn locate_executable(argument: &str) -> Option<PathBuf> {
